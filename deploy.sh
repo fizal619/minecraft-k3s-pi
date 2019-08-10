722cb9e4007617d671ae1c7ae14bece25efe5747
Fizal Sarif <fizal.sarif@gmail.com>
2019-08-10 03:06:55 -0400

    changed deploy to just run restart

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ssh $SU@$SH \
-  'cd ~/minecraft; git pull origin master; sudo systemctl stop minecraft; sudo systemctl start minecraft'
+  'cd ~/minecraft; git pull origin master; ./restart.sh'

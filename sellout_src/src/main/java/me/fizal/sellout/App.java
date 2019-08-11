d6769b4e3381f3b1bffc2a8689a4a36132f0b941
Fizal Sarif <fizal.sarif@gmail.com>
2019-08-10 21:05:05 -0400

    sellout src

---
+++ sellout_src/src/main/java/me/fizal/sellout/App.java
@@ -0,0 +1,16 @@
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class App
+{
+    //  @EventHandler
+    //  public void onPlayerJoin(PlayerJoinEvent event)
+    //  {
+    //      Bukkit.broadcastMessage("Welcome to Jamrock!");
+    //  }
+    public static void main(){
+        System.out.println("TEST");
+    }
+}

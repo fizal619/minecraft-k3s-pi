61095aa64b2e3a968ad1958a824a32f05d77c614
Fizal Sarif <fizal.sarif@gmail.com>
2019-12-21 14:53:49 -0500

    trying to update paper

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ssh $SU@$SH \
-  'cd ~/minecraft; git checkout .; git pull origin master; ./stop.sh; ./start.sh; exit'
+  'cd ~/minecraft; git checkout .; git pull origin master; ./stop.sh; curl --output paperclip.jar -L https://papermc.io/api/v1/paper/1.15.1/25/download; ./start.sh; exit'

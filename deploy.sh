061bb8961f011ee01dabbeb3d5c00755bdc0f3aa
Fizal Sarif <fizal.sarif@gmail.com>
2019-12-21 15:02:17 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ssh $SU@$SH \
+ssh -o StrictHostKeyChecking=no $SU@$SH \
   'cd ~/minecraft; git checkout .; git pull origin master; ./stop.sh; curl --output paperclip.jar -L https://papermc.io/api/v1/paper/1.15.1/25/download; ./start.sh; exit'
